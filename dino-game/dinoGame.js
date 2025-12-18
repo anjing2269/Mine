1801e746381090354a773641b83dd8f569f0bfb9
anjing2269 <guijieyanjing26@gmail.com>
2025-12-18 16:08:34 +0900

    Update dinoGame.js

--- dino-game/dinoGame.js
+++ dino-game/dinoGame.js
@@ -16,6 +16,8 @@ const game = {
   timer: null
 };
 game.bgm1.loop = true;
+game.bgm1.volume = 0.2;
+game.bgm2.volume = 0.2;
 
 // 複数画像読み込み
 let imageLoadCounter = 0;
@@ -57,9 +59,7 @@ function start() {
   game.state = 'gaming';
   createDino();
   game.bgm1.play();
-  game.bgm1.volume = 0.5;
-  console.log(game.bgm1.volume);
-  console.log(game.bgm1.muted);
+  console.log(`bgmの音量 ${game.bgm1.volume}`);
   game.timer = setInterval(ticker, 30);
 }
 
@@ -233,13 +233,14 @@ document.onkeydown = function (e) {
   if (e.code === 'Space' && game.dino.moveY === 0 && game.state === 'gaming') {
     game.dino.moveY = -41;
     game.bgm2.play();
-    game.bgm2.volume = 0.5;
+    console.log(`効果音の音量　${game.bgm2.volume}`);
   }
   if (e.code === 'Enter' && game.state === 'gameover') {
     start();
   }
 
 };
+
 
 
 
